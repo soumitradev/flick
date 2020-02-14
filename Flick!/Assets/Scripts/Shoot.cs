f8fc9fdd63ae0cbf40c9f1bd9294edd77c3acc11
soumitradev <soumitra.shewale.dev@gmail.com>
2020-02-14 23:10:06 +0530

    I did it
    Logic and reading documentation. I have finally solved the unsolvable.

--- Flick!/Assets/Scripts/Shoot.cs
+++ Flick!/Assets/Scripts/Shoot.cs
@@ -51,32 +51,41 @@ public class Shoot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
         // spawn = false;
 
         startPos = transform.position;
+        Invoke("MakeStatic", 0.1f);
+        Invoke("MakeDynamic", 0.1f);
 
-        Invoke("AllowPhysics", .1f);
-        //GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
 
         trail.time = Mathf.Infinity;
     }
 
-    private void AllowPhysics()
+    private void MakeStatic()
     {
-        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
     }
 
-        private void AllowPhysics1()
+    private void MakeDynamic()
     {
         GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    private void MakeKinematic()
+    {
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
     }
 
     // Update is called once per frame
     public void OnEndDrag(PointerEventData pointerEvent)
     {
+
+        
+        Invoke("MakeKinematic", 0.1f);
+        Invoke("MakeDynamic", 0.1f);
+
         //spawn = true;
 
         turnDone = true;
         
 
-        // GetComponent<Rigidbody2D>().isKinematic = false;
 
         GetComponent<CircleCollider2D>().enabled = true;
 
@@ -86,18 +95,13 @@ public class Shoot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
 
         // Add the Force
 
-        Invoke("AllowPhysics1", .1f);
 
         Vector3 dir = startPos - transform.position;
 
         transform.position = startPos;
 
         GetComponent<Rigidbody2D>().AddForce(dir * force);
-        
-        Invoke("AllowPhysics", .1f);
-        
-        Invoke("AllowPhysics1", .1f);
-        // GetComponent<Rigidbody2D>().isKinematic = true;
+    
 
         FindObjectOfType<AudioManager>().Play("Click");
 
@@ -119,55 +123,52 @@ public class Shoot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
     }
 
     private void FixedUpdate()
-    {
-        if (GetComponent<Speed>().speed > 0 && !stateStress)
-            Instantiate(ball, transform.position, Quaternion.identity);
-    }
-
-    private void Update()
-    {
-        if (!stateStress)
+    {   if (!stateStress)
             startPos = transform.position;
 
-        if (!stateStress && GetComponent<Speed>().speed < 0.001f && !printed)
+        if (!stateStress && GetComponent<Speed>().speed < 0.01f && !printed)
         {
             stateStress = false;
 
             Instantiate(ball, startPos, Quaternion.identity);
 
             printed = true;
-
             // spawn = false;
 
             // GetComponent<Rigidbody2D>().isKinematic = true;
         }
 
-        if (!stateStress && GetComponent<Speed>().speed < 0.001f)
+        if (!stateStress && GetComponent<Speed>().speed < 0.01f)
         {
-            // GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static; 
-            Invoke("AllowPhysics", .1f);
-            Invoke("AllowPhysics1", .1f);
+            MakeStatic();
+            MakeDynamic();
         }
+
+        if (GetComponent<Speed>().speed > 0 && !stateStress)
+        {
+            Vector3 new_pos = transform.position;
+            new_pos[2] = 1;
+            Instantiate(ball, new_pos, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
     }
 
 
     public void OnDrag(PointerEventData eventData)
     {
-        GetComponent<CircleCollider2D>().enabled = false;
 
-        // Invoke("AllowPhysics", .1f);
-
-        // GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-
-        //  GetComponent<Rigidbody2D>().isKinematic = false;
-
-        // Convert mouse position to world position
-
-        Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // Keep it in a certain radius
         if (!turnDone)
         {
+            GetComponent<CircleCollider2D>().enabled = false;
+
+        // Convert mouse position to world position
+
+            Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             float radius = 1.8f;
 
             Vector3 dir = p - startPos;
